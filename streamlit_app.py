cf8b1eade7d993e273ee9e9514d8706d1d412699
Najmi-Rk <najmi.rakhmapradyptaa@gmail.com>
2025-05-27 11:52:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.title("Nastars-Bakery🥨🌸")
 st.write("Find your sweet food at this place (*^ω^)")
 st.image("dc50297e933a6c84a806232607af4a73.jpg")
+st.write("Choose Your Favorite Cake")
 st.image("3de28c91a675305ab3f05292ef85682c.jpg")
 st.image("c8b7dc90b245c51fbff7ed2703a63599 (1).jpg")
 
