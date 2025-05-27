c5b1cf7a6e431e77ff865cf149f2ceb75e42e231
Najmi-Rk <najmi.rakhmapradyptaa@gmail.com>
2025-05-27 11:58:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-st.title("Nastars-Bakery
-🥨🍒")
+st.title("Nastars-Bakery  🥨🍒")
 st.write("Find your sweet food at this place (*^ω^)")
 st.image("dc50297e933a6c84a806232607af4a73.jpg")
-st.write("Choose Your Favorite Cake")
-
+st.write("Choose Your Favorite Cake(*≧ω≦)")
 st.image("3de28c91a675305ab3f05292ef85682c.jpg")
+st.write("Choose Your Favorite Fruit(*⌒∇⌒*)")
 st.image("c8b7dc90b245c51fbff7ed2703a63599 (1).jpg")
+st.write("Choose Your Favorite Pie(*^O^*)")
 
 st.title("Cek Bilangan Ganjil/Genap")
 st.header("mengecek nilai genap/ganjil")
